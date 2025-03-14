93d8a468500dc346ce2dbb71cb9f965466c5c215
RugeFX <zackfxg@gmail.com>
2025-03-14 23:27:57 +0700

    init: initial project

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Image MyImage;
+    public Slider MySlider;
+
+    public Sprite MySprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyImage.sprite = MySprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyImage.color = new Color(0, 0, 0, MySlider.value);
+    }
+}
