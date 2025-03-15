4936b34821d19886998937beb89967b6cfd26dc7
RugeFX <zackfxg@gmail.com>
2025-03-16 02:27:52 +0700

    feat: image slider

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,24 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public Image MyImage;
+    private Image MyImage;
     public Slider MySlider;
     public Button MyButton;
-
     public Sprite MySprite;
+
+    private void HandleClick()
+    {
+        MyImage.enabled = !MyImage.enabled;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        MyButton.onClick.AddListener(HandleClick);
+
+        MyImage = GetComponentsInChildren<Image>().Single((image) => image.CompareTag("MenuImage"));
+
         MyImage.sprite = MySprite;
+        MyImage.color = Color.white;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MyImage.color = new Color(0, 0, 0, MySlider.value);
+        MyImage.color = new Color(1, 1, 1, MySlider.value);
     }
 }
